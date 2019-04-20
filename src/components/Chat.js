d2efe144ca7a365ca69ae8abef63525d8363eb00
Bruno Talhate <btalhate@gmail.com>
2019-04-20 16:51:33 -0300

    added android status bar color

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import PropTypes from 'prop-types'
 import { messagePropTypes } from '../constants/propTypes'
 import { MessagesList } from './MessagesList'
 import { FooterInput } from './FooterInput'
 import { KeyboardHandlingView } from './Shared/KeyboardHandlingView'
+import { theme } from '../constants/theme'
 
 export function Chat({ messages, onSendMessage }) {
   return (
     <KeyboardHandlingView
       render={isKeyboardActive => (
         <React.Fragment>
+          <StatusBar backgroundColor={theme.colors.primaryDark} />
           <MessagesList messages={messages} />
           <FooterInput
             onSendMessage={onSendMessage}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from './config/redux'
 
+import { createStore } from './config/redux'
 import { Navigator } from './Navigator'
 
 const store = createStore()
